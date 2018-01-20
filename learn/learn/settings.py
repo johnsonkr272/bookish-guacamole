4f52a5e2147bd101a6966b584e3776f497cdc7b1
johnsonkr272 <johnson.kyle272@gmail.com>
2018-01-20 00:52:34 +0000

    Moved secret key to secret location...

--- learn/learn/settings.py
+++ learn/learn/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '192.241.141.47'
+]
 
 
 # Application definition
